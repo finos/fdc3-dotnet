389c955793a3917ab17b80329837a9cec7efead1
bingenito <28159742+bingenito@users.noreply.github.com>
2023-02-01 06:23:33 -0500

    Disable warning for code analysis empty interface

--- src/Fdc3/IIntentResult.cs
+++ src/Fdc3/IIntentResult.cs
@@ -16,10 +16,12 @@ using MorganStanley.Fdc3.Context;
 
 namespace MorganStanley.Fdc3
 {
+#pragma warning disable CA1040
     /// <summary>
     /// Intents can return results that are either context data objects or a reference to a Channel.
     /// </summary>
     public interface IIntentResult {
         // Marker interface
     }
+#pragma warning restore CA1040
 }
