c1967088378dfbe171abbdfca99e1a44570198ea
bingenito <28159742+bingenito@users.noreply.github.com>
2024-07-19 15:27:20 -0400

    Changer Chart OtherConfig from array to IEnumerable

--- src/Fdc3/Context/Chart.cs
+++ src/Fdc3/Context/Chart.cs
@@ -12,11 +12,13 @@
  * and limitations under the License.
  */
 
+using System.Collections.Generic;
+
 namespace Finos.Fdc3.Context
 {
     public class Chart : Context, IContext
     {
-        public Chart(Instrument[] instruments, TimeRange? range = null, IContext[]? otherConfig = null, string? style = null, object? id = null, string? name = null)
+        public Chart(Instrument[] instruments, TimeRange? range = null, IEnumerable<IContext>? otherConfig = null, string? style = null, object? id = null, string? name = null)
             : base(ContextTypes.Chart, id, name)
         {
             this.Instruments = instruments;
@@ -27,7 +29,7 @@ namespace Finos.Fdc3.Context
 
         public Instrument[] Instruments { get; set; }
         public TimeRange? Range { get; set; }
-        public IContext[]? OtherConfig { get; set; }
+        public IEnumerable<IContext>? OtherConfig { get; set; }
         public string? Style { get; set; }
 
         object? IContext<object>.ID => base.ID;
