725c5465977eded69fc7270804b08b464e04abe1
bingenito <brian.ingenito@morganstanley.com>
2025-07-30 08:21:47 -0400

    Support broadcast in FDC3 Action

--- src/Fdc3/Context/Action.cs
+++ src/Fdc3/Context/Action.cs
@@ -16,19 +16,23 @@ namespace Finos.Fdc3.Context
 {
     public class Action : Context, IContext
     {
-        public Action(string title, IContext context, string? intent, IAppIdentifier? app = null, object? id = null, string? name = null)
+        public Action(string title, IContext context, string? intent, IAppIdentifier? app = null, string? action = null, string? channelId = null, object? id = null, string? name = null)
             : base(ContextTypes.Action, id, name)
         {
             this.Title = title;
             this.Context = context;
             this.Intent = intent;
             this.App = app;
+            this.ActionType = action;
+            this.ChannelId = channelId;
         }
 
         public string Title { get; private set; }
         public IContext Context { get; private set; }
         public string? Intent { get; private set; }
         public IAppIdentifier? App { get; private set; }
+        public string? ActionType { get; private set; }
+        public string? ChannelId { get; private set; }
 
     }
 }
